bcdb79973dbaec546b1a1338b2988e1d4d80f1b9
Vesa <vesainwonderland@gmail.com>
2021-11-24 23:41:02 +0200

    4.5.14 tuotteen poistaminen tyhjentaa korin

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -96,3 +96,11 @@ class TestOstoskori(unittest.TestCase):
         ostos = self.kori.ostokset()[0]
         self.assertEqual(ostos.lukumaara(), 1)
         self.assertEqual(ostos.tuotteen_nimi(), "Maito")
+
+    def test_yhden_tuotteen_sisaltavasta_korista_poistetaan_tuote(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        self.kori.poista_tuote(maito)
+
+        self.assertEqual(self.kori.hinta(), 0)
+        self.assertEqual(self.kori.tavaroita_korissa(), 0)
