a08730df77cff54d4750eb352a4aa61e7e35611c
Vesa <vesainwonderland@gmail.com>
2021-11-24 23:06:11 +0200

    4.5.10 kahden eri tuotteen jalkeen kaksi ostosta

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -60,3 +60,12 @@ class TestOstoskori(unittest.TestCase):
         ostos = self.kori.ostokset()[0]
         self.assertEqual(ostos.lukumaara(), 1)
         self.assertEqual(ostos.tuotteen_nimi(), "Maito")
+
+    def test_kahden_tuotteen_lisaamisen_jalkeen_korissa_kaksi_ostosoliota(self):
+        maito = Tuote("Maito", 3)
+        omena = Tuote("Omena", 1)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(omena)
+
+        ostokset = self.kori.ostokset()
+        self.assertEqual(len(ostokset), 2)
